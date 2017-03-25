5ed899f68f27c1b706cdd9bd3f86b5d3e3ea3092
Chris Alexander <chris@chris-alexander.co.uk>
2017-03-25 18:14:54 +0000

    geolocation

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,29 @@
 function Map() {
+    var defaultLocation = { lat: 51.512416, lng: -0.114123};
+
     var map = undefined;
     var auth = undefined;
 
+    var updateLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (pos) => handleLocation({ lat: pos.coords.latitude, lng: pos.coords.longitude }),
+                () =>  handleLocationError("Geo API errored")
+            );
+        } else {
+            handleLocationError("Geo API not supported");
+        }
+    }
+    var handleLocation = (loc) => {
+        map.setCenter(loc);
+        map.setZoom(14);
+    };
+    var handleLocationError = (error) => console.error(error);
+
     this.init = () => {
         map = new google.maps.Map(document.querySelector("#map"), {
-            center: { lat: 51.318998, lng: -0.678466 },
-            zoom: 12,
+            center: defaultLocation,
+            zoom: 11,
             scrollwheel: true,
             zoomControl: true,
             mapTypeControl: false,
@@ -16,6 +34,8 @@ function Map() {
 
         auth = new Auth();
         map.controls[google.maps.ControlPosition.TOP_RIGHT].push(auth.getContainer());
+
+        updateLocation();
     };
 }
 
